09255194c06f5cb1c9e54df40f9fcf4ed3db3e66
mohammadjavad <mohammadjavada78@gmail.com>
2023-11-20 17:12:14 +0330

    update import

--- src/app/(main)/exhibitors/landing/page.tsx
+++ src/app/(main)/exhibitors/landing/page.tsx
@@ -1,4 +1,5 @@
-import ExhibitorLandingPage from "@/page/ExhibitorLandingPage";
+// import ExhibitorLandingPage from "@/page/ExhibitorLandingPage";
+import ExhibitorLandingPage from "@/page/exhibitorLandingPage";
 import React from "react";
 
 export default function page() {
